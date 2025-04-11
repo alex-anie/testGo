780c121669e7a66a24fb12b549fb6ed61e573b4d
alex-anie <ocxigin@gmail.com>
2025-04-11 13:20:53 +0100

    second commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://127.0.0.1:5500',
+    specPattern: 'cypress/e2e/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: true,
+    screenshotOnRunFailure: true
   },
-});
+})
